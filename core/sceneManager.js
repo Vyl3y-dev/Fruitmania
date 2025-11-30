910c01e1541ce8d318e601e5e8c648e9556daf26
Vy <vcmunos@proton.me>
2025-11-30 08:44:59 -0500

    sceneManager created

---
+++ core/sceneManager.js
@@ -0,0 +1,38 @@
+// create a drawer for all scenes, ref which one is active
+export class SceneManager {
+    constructor() {
+        this.scenes = {};
+        this.currentScene = null;
+    }
+
+// helps know what exists
+    register(name, sceneClass) {
+        this.scenes[name] = new sceneClass();
+    }
+
+// clean transitions, fresh starts, no leftover objects from old scenes
+    changeScene(name){
+        if (this.currentScene && this.currentScene.cleanup) {
+            this.currentScene.cleanup();
+        }
+
+        this.currentScene = this.scenes[name];
+
+        if (this.currentScene && this.currentScene.init) {
+            this.currentScene.init();
+        }
+    }
+
+// updates/draws to be added to loop
+    update(dt) {
+        if (this.currentScene && this.currentScene.update) {
+            this.currentScene.update(dt);
+        }
+    }
+
+    draw(ctx) {
+        if (this.currentScene && this.currentScene.draw) {
+            this.currentScene.draw(ctx);
+        }
+    }
+}
