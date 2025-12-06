84cafd31b5b780ef6d907ad0d1862f505c18f1db
Vy <vcmunos@proton.me>
2025-12-06 04:25:47 -0500

    updated ui gameplay

--- scenes/game.js
+++ scenes/game.js
@@ -2,7 +2,8 @@ export class GameScene {
     init() {
         this.root = document.getElementById("game");
         this.root.innerHTML = `
-            <div id="gameplay" class="gameplay-area">
+            <div id="gameplay" class="">
+            <div id="gameplayBG"></div>
             <div id="vyley" class="character"></div>
             <div id="bluebrick" class="platform"></div>
             <div id="oni" class="character"></div>
